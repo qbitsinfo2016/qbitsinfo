665fc650755137951939c5565c6ab1c4c83a23a9
qbitsinfo2016 <qbitsproject@gmail.com>
2016-04-15 04:01:44 -0300

    Secuencial 6

---
+++ solucions6.py
@@ -0,0 +1,4 @@
+artPrecioComprado = float (input ("Ingrese el precio del articulo que ha comprado: "))
+ganancia30 = artPrecioComprado * 0.30
+precioConGanancia30 = artPrecioComprado + ganancia30
+print "Para obtener una ganancia del 30 por ciento, usted debera vender el producto al siguiente precio: ", precioConGanancia30
