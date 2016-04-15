8a0319dceb831a5eb4c9ba29ac61f06ee83e383c
qbitsinfo2016 <qbitsproject@gmail.com>
2016-04-15 04:00:49 -0300

    Secuencial 5

---
+++ solucions5.py
@@ -0,0 +1,7 @@
+presupuesto = float (input("Ingrese el presupuesto destinado al hospital: "))
+odontologia = presupuesto * 0.40
+traumatologia = presupuesto * 0.30
+pediatria = presupuesto * 0.30
+print "El presupuesto destinado a odontologia sera de: ", odontologia
+print "El presupuesto destinado a traumatologia sera de: ", traumatologia
+print "El presupuesto destinado a pediatria sera de: ", pediatria 
