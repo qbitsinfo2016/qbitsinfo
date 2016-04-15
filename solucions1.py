f28f656844abd1ebe01875daf6eda15e8c3fc493
qbitsinfo2016 <qbitsproject@gmail.com>
2016-04-15 03:56:46 -0300

    Secuencial 1

---
+++ solucions1.py
@@ -0,0 +1,4 @@
+anfut = int (input ("Ingrese el ano futuro: "))
+annac = int (input ("Ingrese su ano de nacimiento: "))
+edadFutura = anfut - annac
+print "su edad en el ano ", anfut ," sera: ", edadFutura
