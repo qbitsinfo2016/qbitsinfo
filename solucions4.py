d116f4c86f4531d06d1d8e53b6c7dd6ed72775dc
qbitsinfo2016 <qbitsproject@gmail.com>
2016-04-15 04:00:01 -0300

    Secuencial 4

---
+++ solucions4.py
@@ -0,0 +1,3 @@
+salarioAnt = float (input("Ingrese su salario antiguo: "))
+salarioNuevo = salarioAnt + salarioAnt * 0.25
+print "Su salario actualizado con el incremento de un 25 por ciento es de ", salarioNuevo
